dd256b7a03b5375f49e682feb6a2bb507a2e988c
wickramDSK <wickdsk.2k@gmail.com>
2024-11-05 12:07:33 +0530

    Update StudentController.java

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,5 +1,52 @@
 package com.example.demo.controller;
 
-public class StudentController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin(origins = "*")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student) {
+        return new ResponseEntity<Student>(studentService.saveStudent(student), HttpStatus.CREATED);
+    }
+
+    // GetAll Rest Api
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    // Get by Id Rest Api
+    @GetMapping("{id}")
+    // localhost:8080/api/students/1
+    public ResponseEntity<Student> getStudentById(@PathVariable("id") long studentID){
+        return new ResponseEntity<Student>(studentService.getStudentById(studentID),HttpStatus.OK);
+    }
+
+    // Update Rest Api
+    @PutMapping("{id}")
+    public ResponseEntity<Student> updateStudent (@PathVariable("id") long id, @RequestBody Student student){
+        return new
+        ResponseEntity<Student>(studentService.updateStudent(student,id),HttpStatus.OK);
+    }
+
+    // Delete Rest Api
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") long id){
+        //delete student from db
+        studentService.deleteStudent(id);
+        return new ResponseEntity<String>("Student deleted Successfully.",HttpStatus.OK);
+}
 }
